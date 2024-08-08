3fef0c35ad7f23733f5a3b9a5c20f01e61d7f647
Takkino31 <takkinoyaya@gmail.com>
2024-08-08 11:49:41 +0000

    Initialization Project && Creation Auto Database

---
+++ src/main/java/takkino/learning/lonko/LonkoApplication.java
@@ -0,0 +1,13 @@
+package takkino.learning.lonko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LonkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LonkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/takkino/learning/lonko/LonkoApplicationTests.java
@@ -0,0 +1,13 @@
+package takkino.learning.lonko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LonkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
